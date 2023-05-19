f812d5ff5a7ad2bf1b0d8fbf8fd4f46f3f00f78b
Don1k1337 <dlyaspamnyhpisem@gmail.com>
2023-05-19 22:39:03 +0600

    fix: Issue with refreshing page fixed. Layout improved.

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,43 +1,56 @@
 <template>
   <div class="task-content">
     <task-edit class="task-edit" :task="resolvedTask" v-if="resolvedTask && !loading" :key="taskId"/>
-    <p v-else>Loading task...</p>
+    <p v-else-if="loading">Loading task...</p>
+    <not-found-view v-else/>
   </div>
 </template>
 
 <script>
 import Card from '@/components/ui/Card/Card.vue';
 import TaskEdit from '@/components/Task/TaskEdit.vue';
-import {mapGetters, mapState} from 'vuex';
+import { mapGetters, mapState } from 'vuex';
+import NotFoundView from "@/views/NotFoundView.vue";
 
 export default {
   name: 'EditView',
   components: {
+    NotFoundView,
     Card,
     TaskEdit,
   },
   computed: {
     ...mapGetters(['retrieveTaskById']),
-    ...mapState(['tasks', 'loading']),
+    ...mapState(['loading']),
     taskId() {
-      return this.$route.params.id;
+      const route = this.$route.params.id;
+      return parseInt(route);
     },
     resolvedTask() {
-      return this.retrieveTaskById(this.taskId);
+      const task = this.retrieveTaskById(this.taskId);
+      if (task) {
+        return task;
+      } else {
+        return null; // Task not found
+      }
     },
   },
-  async mounted() {
-    await this.loadData();
+  async created() {
+    try {
+      await this.loadData();
+    } catch (error) {
+      console.error(error);
+    }
   },
   methods: {
     async loadData() {
       await this.$store.commit('SET_LOADING', true);
       try {
-        return await this.$store.dispatch('retrieveTaskById', this.taskId)
+        await this.$store.dispatch('retrieveTasksFromLS');
       } catch (e) {
         console.error(e);
       } finally {
-        await this.$store.commit('SET_LOADING', false);
+        this.$store.commit('SET_LOADING', false);
       }
     },
   },
@@ -45,10 +58,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 .task-content p {
   color: #ccc;
   margin-top: 10rem;
 }
-
 </style>
